e4b1f8456952ee59d892e6325b20c9b8193fc650
Arnold Roy <royarnold647@gmail.com>
2025-07-11 19:23:04 +0530

    d

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arnold {};
+
+    arnold.push_back(99);
+   arnold.push_back(55);
+    int n= arnold.size();
+
+    for(int i=0;i<n;i++){
+         cout<<arnold[i];
+    }
+    return 0;
+
+}
