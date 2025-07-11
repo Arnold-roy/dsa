a9f105580da935af6f7e5c6a39bc8a3a68011d85
Arnold Roy <royarnold647@gmail.com>
2025-07-11 20:44:29 +0530

    mediam

---
+++ Median.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    float ans;
+    if (n % 2 == 0)
+    {
+        ans = float((arr[n / 2]) + (arr[n / 2 - 1] / 2.0));
+        cout << "median" << ans << endl;
+    }
+    else
+    {
+        ans = arr[n / 2];
+        cout << "median" << ans << endl;
+    }
+}
